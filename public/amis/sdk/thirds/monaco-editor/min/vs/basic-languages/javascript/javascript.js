b40195e12fd1edeb0610b41d48bf1be3894ce4f9
wuduoyi <wuduoyi@baidu.com>
2021-04-09 16:06:27 +0800

    更新 amis 版本到 1.1.6-beta.10

--- public/amis/sdk/thirds/monaco-editor/min/vs/basic-languages/javascript/javascript.js
+++ public/amis/sdk/thirds/monaco-editor/min/vs/basic-languages/javascript/javascript.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-languages version: 2.1.1(425b0e3c9ea2067410b8f90e0f30de383188c418)
+ * monaco-languages version: 2.3.0(57af10ae0184db4e0f7f9a92ff972629c39ccb53)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-languages/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
