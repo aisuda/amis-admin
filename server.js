071438b48a278467ad90aa3b07605475bf8b9c5a
2betop <2betop.cn@gmail.com>
2021-05-25 22:15:30 +0800

    改成 browserHistory

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
-const reload = require("reload");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const reload = require('reload');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
 
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
-app.use(logger("dev"));
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
 app.use(bodyParser.json()); // Parses json, multi-part (file), url-encoded
 
-app.use("/public", express.static("public"));
-app.use("/pages", express.static("pages"));
+app.use('/public', express.static('public'));
+app.use('/pages', express.static('pages'));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const server = http.createServer(app);
@@ -26,15 +26,15 @@ reload(app)
     // reloadReturned is documented in the returns API in the README
 
     // Reload started, start web server
-    server.listen(app.get("port"), function () {
+    server.listen(app.get('port'), function () {
       console.log(
-        "Web server listening on port http://localhost:" + app.get("port")
+        'Web server listening on port http://localhost:' + app.get('port')
       );
     });
   })
   .catch(function (err) {
     console.error(
-      "Reload could not start, could not start server/sample app",
+      'Reload could not start, could not start server/sample app',
       err
     );
   });
